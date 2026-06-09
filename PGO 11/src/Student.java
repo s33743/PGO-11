c20807f8a68e88e166202ec00214c3c036f07ca1
s33743 <s33743@pjwstk.edu.pl>
2026-06-09 21:05:27 +0200

    Klasy LoyaltyDiscountPolicy i Student

---
+++ PGO 11/src/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+
+    private String id;
+    private String fullName;
+    private String group;
+    private int loyaltyPoints;
+
+    public Student(String id, String fullName, String group) {
+        this.id = id;
+        this.fullName = fullName;
+        this.group = group;
+        this.loyaltyPoints = 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public void addPoints(int points) {
+        loyaltyPoints += points;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + fullName + " (" + loyaltyPoints + " pkt)";
+    }
+}
